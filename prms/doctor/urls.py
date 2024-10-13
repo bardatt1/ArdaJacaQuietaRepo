e9e185dc11d107edc12933314286c3b1c3c82a6a
bardatt1 <153211702+bardatt1@users.noreply.github.com>
2024-10-14 02:18:20 +0800

    Delete prms directory

--- prms/doctor/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('register/', views.doctor_register_view, name='registration'),
-    path('login/', views.doctor_login_view, name='login'),
-    path('home/', views.doctor_home_view, name='home'),
-    path('add-patient/', views.add_patient_view, name='add_patient'),
-]
